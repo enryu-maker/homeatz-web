35b7cbd2685751e44836bb2f06e60f12441bb1ee
enryu-maker <akifkhan60067@gmail.com>
2024-07-07 22:58:35 +0530

    cc

--- src/Component/ChefInfoRedirect.js
+++ src/Component/ChefInfoRedirect.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
 export default function ChefInfoRedirect() {
-
+    const state = useLocation()
     React.useEffect(() => {
-        const chefId = window.location.pathname.split("/")[2]; // Assuming /chefinfo/9 format
+        const chefId = state.pathname.split("/")[2]; // Assuming /chefinfo/9 format
+        console.log(chefId)
         const deepLinkUrl = `homeatz://chefinfo/${chefId}`;
         window.open(deepLinkUrl, "_self"); // Use _self to open in the same window/tab
     }, []);
